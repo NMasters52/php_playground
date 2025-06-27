f86ec3ce2d0dd91d00f1ed9d32b807f26b7fe75c
nmasters <nmasters52@gmail.com>
2025-06-27 17:16:00 -0400

    restart

--- index.php
+++ index.php
@@ -9,42 +9,7 @@
 <body>
     
 <?php
-   
-	$products = [
-	    [
-	        "id" => 1,
-	        "name" => "MacBook Pro",
-	        "price" => 1299.99,
-	        "category" => "Electronics",
-	        "image" => "macbook.jpg",
-	        "inStock" => true,
-	        "rating" => 4.8
-	    ],
-	    [
-	        "id" => 2,
-	        "name" => "Coffee Mug",
-	        "price" => 12.99,
-	        "category" => "Home",
-	        "image" => "mug.jpg",
-	        "inStock" => false,
-	        "rating" => 4.2
-	    ]
-	];
 
-
-    function displayProductCard($products) {
-        foreach($products as $product) {
-            echo "
-            <h1 class='text-red-200'>{$product['name']}</h1>
-            ";
-        }
-    }
-
-    echo displayProductCard($products);
-
-
-
-  
 
 ?>
 
