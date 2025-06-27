c52e4057ee7c586c2dcc974323e641da2e1683cd
nmasters <nmasters52@gmail.com>
2025-06-27 16:48:53 -0400

    component practice

--- functional/functional.php
+++ functional/functional.php
@@ -5,15 +5,24 @@
 
 <?php
 
-$products = [
-    ["name" => "Laptop", "price" => 999, "category" => "electronics"],
-    ["name" => "Shirt", "price" => 25, "category" => "clothing"],
-    ["name" => "Phone", "price" => 699, "category" => "electronics"],
-    ["name" => "Jeans", "price" => 80, "category" => "clothing"]
+// $products = [
+//     ["name" => "Laptop", "price" => 999, "category" => "electronics"],
+//     ["name" => "Shirt", "price" => 25, "category" => "clothing"],
+//     ["name" => "Phone", "price" => 699, "category" => "electronics"],
+//     ["name" => "Jeans", "price" => 80, "category" => "clothing"]
+// ];
+
+// function filterByCategory($products, $category) {
+//     return array_filter($products, fn($product) => $product["category"] === $category);
+// }
+
+$users = [
+    ["first_name" => "John", "last_name" => "Doe", "age" => 25],
+    ["first_name" => "Jane", "last_name" => "Smith", "age" => 30]
 ];
 
-function filterByCategory($products, $category) {
-    return array_filter($products, fn($product) => $product["category"] === $category);
+function getFullNames($users) {
+    return array_map(fn($user) => $user["first_name"] . ' ' . $user["last_name"] ,$users);
 }
 
 ?>
--- index.php
+++ index.php
@@ -9,12 +9,66 @@
 <body>
     
 <?php
-    require './functional/functional.php';
-    $electronics = filterByCategory($products, 'electronics');
-    foreach ($electronics as $product) {
-        echo $product['name'] . "<br>";
+   
+	$products = [
+	    [
+	        "id" => 1,
+	        "name" => "MacBook Pro",
+	        "price" => 1299.99,
+	        "category" => "Electronics",
+	        "image" => "macbook.jpg",
+	        "inStock" => true,
+	        "rating" => 4.8
+	    ],
+	    [
+	        "id" => 2,
+	        "name" => "Coffee Mug",
+	        "price" => 12.99,
+	        "category" => "Home",
+	        "image" => "mug.jpg",
+	        "inStock" => false,
+	        "rating" => 4.2
+	    ]
+	];
+
+
+    class ProductCard {
+       public $props = [];
+
+       public function __construct($props)
+       {
+        $this->props = $props;
+       }
+
+       public function render() {
+            $product = $this->props['product'];
+            $showProduct = $this->props['showCategory'];
+            $html = "<h3>{$product['name']}</h3>";
+            
+            if ($showProduct) {
+                $stockText = $product['inStock'] ? 'in stock' : 'out of stock';
+                $html .= "<p>{$stockText}</p>";
+            }
+            return $html;
+        }
     }
-    print_r($electronics);
+        
+    $cardContent = new ProductCard(['product'=>$products[0], 'showCategory'=>false]);
+    echo $cardContent->render();
+
 ?>
+
+<br/>
+
+<div class="border-2 border-black-500 w-1/2 mx-auto p-2">
+    <div class="border-2 border-red-200">
+        <img src="" alt="">
+    </div>
+    <p>Name</p>
+    <p>price</p>
+    <p>catagory</p>
+    <p>In Stock</p>
+</div>
+
 </body>
 </html>
